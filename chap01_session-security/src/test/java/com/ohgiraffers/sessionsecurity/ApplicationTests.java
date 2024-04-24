402844f5055c9fb8394825cdd5330f429640d0ef
SOOBIN\tifkr <soobinnunu1101@gmail.com>
2024-04-24 14:21:41 +0900

    0424 commit

---
+++ chap01_session-security/src/test/java/com/ohgiraffers/sessionsecurity/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.sessionsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
